c9608b16942a8c9a46e211d2b5142e045e0eeca3
Abhisesh Acharya <aachary5@u.rochester.edu>
2018-07-30 17:42:55 +0545

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // LIBRARY IMPORTS
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 // LOCAL IMPORTS
@@ -95,6 +96,34 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send();
     });
 });
+
+// Update todo items using the patch route
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
 
 // this is a very basic server
 app.listen(port, () => {
