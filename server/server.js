02bc784899a29a26ba141744681ddf188f0983c3
Abhisesh Acharya <aachary5@u.rochester.edu>
2018-08-01 18:43:05 +0545

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -71,6 +71,19 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+// Route for logging in users
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 // register the GET handler using app.get
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
